f9995fc9e12aabea9efec045ccd9edf827147a19
David Moreno <david@axiombox.com>
2009-07-09 13:33:38 -0500

    added storable objects and 02_second

--- 02-second.pl
+++ 02-second.pl
@@ -1,10 +1,41 @@
 #!/usr/bin/perl
 
 use Modern::Perl;
+use Data::Dumper;
+use Storable;
 
 opendir(my $dir, ".") or die "Couldn't open it :(";
 
 for my $d (readdir($dir)) {
 	next unless $d =~ /^___/;
-	say $d;
+	
+	open my $fh, $d or die;
+
+	local $/ = undef;
+	my $js = <$fh>;
+	local $/ = "\n";
+	close $fh;
+	
+	my(@a) = $js =~ /s(\d+?)\.(\w+?)=(.+?);/gis;
+	
+	# old C-for! C FTW!
+
+	my $r = [];
+	for(my $i = 0; $i < (scalar(@a)); $i += 3) {
+		my $v = $a[$i + 2];
+		$v =~ s/(^"|"$)//g;
+		$v = undef if $v eq 'null';
+		
+		$r->[$a[$i]]->{$a[$i + 1]} = $v;
+		$r->[$a[$i]]->{id} = $a[$i];
+	}
+
+	for my $t (@$r) {
+		my(@p) = $js =~ /$t->{parameters}\[\d+\]="?(.+?)"?;/gis;
+		$t->{parameters} = \@p;
+		# say $t->{parameters};
+	}
+
+	store $r, '_s_'.$d;
+	
 }
